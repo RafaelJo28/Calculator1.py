2eb11136bace7a083c13018b65d2d86de5ecde6f
RafaelJo28 <jovanney.husni@binus.ac.id>
2024-09-10 22:35:09 +0700

    Update and rename .gitignore to CalculatorSimple.py

---
+++ CalculatorSimple.py
@@ -0,0 +1,21 @@
+a= float(input("Enter Value A: "))
+b= float(input("Enter Value B: "))
+calculator= input("Choose operation: ")
+
+if calculator == "+":
+    print(a + b)
+    
+if calculator == "-":
+    print(a - b)
+    
+if calculator == "x":
+    print(a * b)
+    
+if calculator == "/":
+    print(a / b)
+    
+if calculator == "//":
+    print(a // b)
+    
+if calculator == "remainder":
+    print(a % b)
